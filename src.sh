9e5a78d0fa6280b71c1055e491de44d6a4f8f8a2
Christian Pointner <equinox@spreadspace.org>
2016-02-13 18:35:34 +0100

    src now puts audio and video into seperate fifos

--- src.sh
+++ src.sh
@@ -1,25 +1,13 @@
 #!/bin/sh
 
-OUT=$1
-RATE=$2
-IN=$3
+VFIFO=$1
+AFIFO=$2
 
-if [ -z $OUT ] || [ -n $RATE ]; then
-  echo "Usage: $0 <output> <bitrate> [ <input> ]"
+if [ -z $VFIFO ] || [ -z $AFIFO ]; then
+  echo "Usage: $0 <video-fifo> <audio-fifo>"
+  exit 1
 fi
 
-if [ -z "$IN" ]; then
-  exec ffmpeg -f alsa -i pulse -f video4linux2 -s 1024x576 -i /dev/video0 -pix_fmt yuv420p \
-     -vcodec mpeg2video -s 720x576 -r 25 -flags cgop+ilme -sc_threshold 1000000000 -g 25 -bf 2 -b:v 4M -minrate 4M -maxrate 4M -bufsize 2M \
-     -acodec mp2 -ac 2 -b:a 192k -muxrate $RATE \
-     -metadata service_provider="realraum" \
-     -metadata service_name="realraum DVB-T Test" \
-     -f mpegts pipe: > "$OUT"
-else
-  exec ffmpeg -i "$IN" \
-     -vcodec mpeg2video -s 720x576 -r 25 -flags cgop+ilme -sc_threshold 1000000000 -g 25 -bf 2 -b:v 4M -minrate 4M -maxrate 4M -bufsize 2M \
-     -acodec mp2 -ac 2 -b:a 192k -muxrate $RATE \
-     -metadata service_provider="realraum" \
-     -metadata service_name="realraum DVB-T Test" \
-     -f mpegts pipe: > "$OUT"
-fi
+exec ffmpeg -f alsa -i pulse -f video4linux2 -s 1024x576 -i /dev/video0 -pix_fmt yuv420p \
+     -vcodec mpeg2video -s 720x576 -r 25 -flags cgop+ilme -sc_threshold 1000000000 -g 25 -bf 2 -b:v 4M -minrate 4M -maxrate 4M -bufsize 2M -f mp2 -map 1 pipe:3 \
+     -acodec mp2 -ac 2 -b:a 192k -f mp2 -map 0 pipe:4   3> "$VFIFO" 4> "$AFIFO"
